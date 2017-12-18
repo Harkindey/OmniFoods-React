61ad5806c740aa030e183887d0201032ffeb2bf5
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-12-18 20:27:43 +0100

    the header component part 3

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,6 +3,17 @@ import React  from 'react';
 const Header = () => {
     return (
         <header>
+            <nav>
+                <div className="row">
+                    <img src="./assets/logo-white.png" alt="Omnifood logo" className="logo"/>
+                    <ul className="main-nav">
+                        <li><a href='#'>Food delivery</a></li>
+                        <li><a href="#">How it works</a></li>
+                        <li><a href="#">Our cities</a></li>
+                        <li><a href="#">Sign up</a></li>
+                    </ul>
+                </div>
+            </nav>
             <div className="hero-text-box">
                 <h1>Goodbye junk food.<br/>Hello super healthy meals.</h1>
                 <a href="#" className="btn btn-full">I’m hungry</a>
