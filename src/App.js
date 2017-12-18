6a21b7c7a1d4f4137bd3d5d0c26a2eacdc75cf6d
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-12-18 22:32:35 +0100

    building the features Component.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'normalize.css';
 import './css/App.css';
 import './css/grid.css'
 import Header from './component/Header';
+import Features from './component/Features';
 
 class App extends Component {
   render() {
     return (
        <div>
           <Header />
+          <Features />
        </div>
     );
   }
