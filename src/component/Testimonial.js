9aafb4cd669a24166a2bc9fb07e43adb7b1b2fa8
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-12-19 15:31:30 +0100

    finished the testimonial componnet

--- src/component/Testimonial.js
+++ src/component/Testimonial.js
@@ -8,22 +8,22 @@ const Testimonial = () => {
             </div>
             <div className="row">
                 <div className="col span-1-of-3">
-                    <blackquote>
+                    <blockquote>
                         Omnifood is just awesome! I just launched a startup which leaves me with no time for cooking, so Omnifood is a life-saver. Now that I got used to it, I couldn't live without my daily meals!
                         <cite><img src="./assets/customer-1.jpg" />Alberto Duncan</cite>
-                    </blackquote>
+                    </blockquote>
                 </div>
                 <div className="col span-1-of-3">
-                    <blackquote>
+                    <blockquote>
                         Inexpensive, healthy and great-tasting meals, delivered right to my home. We have lots of food delivery here in Lisbon, but no one comes even close to Omifood. Me and my family are so in love!
                         <cite><img src="./assets/customer-2.jpg" />Joana Silva</cite>
-                    </blackquote>
+                    </blockquote>
                 </div>
                 <div className="col span-1-of-3">
-                    <blackquote>
+                    <blockquote>
                         I was looking for a quick and easy food delivery service in San Franciso. I tried a lot of them and ended up with Omnifood. Best food delivery service in the Bay Area. Keep up the great work!
                         <cite><img src="./assets/customer-3.jpg" />Milton Chapman</cite>
-                    </blackquote>
+                    </blockquote>
                 </div>
             </div>
         </section>
